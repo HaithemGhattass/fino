78d9adb0ca9a954c38aca79772da838273b2554e
HaithemGhattass <haithem.ghattas@esprit.tn>
2026-01-09 00:28:42 +0100

    feat: init app frontend

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/globals.css";
+import "./styles/layout.css";
+
+export default function App() {
+  return (
+    <div className="app-layout">
+      <aside className="sidebar">Fino</aside>
+      <main className="main">App Content</main>
+    </div>
+  );
+}
